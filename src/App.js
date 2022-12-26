dbd26b9cec9f91ee4c76b339f4a89c932c66236f
jnrchry <jnrchry@gmail.com>
2022-12-26 08:54:26 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Topbar from "./components/topbar/Topbar.js";
+import Sidebar from "./components/sidebar/Sidebar.js";
+import "./App.scss";
 
 const App = () => {
   return (
     <div>
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">other pages</div>
+      </div>
     </div>
   );
 };
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./sidebar.scss";
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <h1>Sidebar</h1>
+    </aside>
+  );
+};
+
+export default Sidebar;
