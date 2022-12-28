f1ba3555a4a7e0d9b6729e4fdedccf02fc6f4107
jnrchry <jnrchry@gmail.com>
2022-12-28 04:56:20 +0000

    commit

--- src/pages/user/User.js
+++ src/pages/user/User.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./user.scss";
 import Model from "../../assets/img/model-1.jpg";
-import { PermIdentity } from "@mui/icons-material";
+import {
+  CalendarToday,
+  LocationSearching,
+  MailOutline,
+  PermIdentity,
+  PhoneAndroid,
+  Publish,
+} from "@mui/icons-material";
 
 const User = () => {
   return (
@@ -23,11 +30,106 @@ const User = () => {
           </div>
           <div className="userShowBottom">
             <span className="userShowTitle">Account Details</span>
-            <PermIdentity />
-            <span className="userShowInfoTitle">jnrwahab99</span>
+
+            <div className="userShowInfo">
+              <PermIdentity className="userShowIcon" />
+              <span className="userShowInfoTitle">jnrwahab99</span>
+            </div>
+
+            <div className="userShowInfo">
+              <CalendarToday className="userShowIcon" />
+              <span className="userShowInfoTitle">10.05.1995</span>
+            </div>
+
+            <span className="userShowTitle">Contact Details</span>
+
+            <div className="userShowInfo">
+              <PhoneAndroid className="userShowIcon" />
+              <span className="userShowInfoTitle">+233 548708343</span>
+            </div>
+
+            <div className="userShowInfo">
+              <MailOutline className="userShowIcon" />
+              <span className="userShowInfoTitle">jnrwahab@gmail.com</span>
+            </div>
+
+            <div className="userShowInfo">
+              <LocationSearching className="userShowIcon" />
+              <span className="userShowInfoTitle">Accra | Ghana</span>
+            </div>
           </div>
         </div>
-        <div className="userUpdate"></div>
+        <div className="userUpdate">
+          <span className="userUpdateTitle">Edit</span>
+          <form className="userUpdateForm">
+            <div className="uswerUpdateLeft">
+              <div className="userUpdateItem">
+                <label htmlFor="">Username</label>
+                <input
+                  type="text"
+                  name=""
+                  className="userUpdateInput"
+                  placeholder="juniorwahab99"
+                />
+              </div>
+
+              <div className="userUpdateItem">
+                <label htmlFor="">Full Name</label>
+                <input
+                  type="text"
+                  name=""
+                  className="userUpdateInput"
+                  placeholder="Junior Wahab"
+                />
+              </div>
+
+              <div className="userUpdateItem">
+                <label htmlFor="">Email</label>
+                <input
+                  type="text"
+                  name=""
+                  className="userUpdateInput"
+                  placeholder="jnrwahab@gmail.com"
+                />
+              </div>
+
+              <div className="userUpdateItem">
+                <label htmlFor="">Phone</label>
+                <input
+                  type="text"
+                  name=""
+                  className="userUpdateInput"
+                  placeholder="+233 558708343"
+                />
+              </div>
+
+              <div className="userUpdateItem">
+                <label htmlFor="">Address</label>
+                <input
+                  type="text"
+                  name=""
+                  className="userUpdateInput"
+                  placeholder="Accra | Ghana"
+                />
+              </div>
+            </div>
+            <div className="uswerUpdateRight">
+              <div className="userUpdateUpload">
+                <img src={Model} alt="" className="userUpdateImg" />
+
+                <label htmlFor="file">
+                  <Publish />
+                </label>
+                <input
+                  type="file"
+                  name=""
+                  id="file"
+                  style={{ display: "none" }}
+                />
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
