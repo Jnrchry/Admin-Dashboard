f170ad6374e861a950d00f64b1d41d0e5648e3f2
jnrchry <jnrchry@gmail.com>
2022-12-28 01:00:08 +0000

    commit

--- src/components/data.js
+++ src/components/data.js
@@ -44,3 +44,75 @@ export const userdata = [
     ActiveUser: 5000,
   },
 ];
+
+export const userRows = [
+  {
+    id: 1,
+    username: "Jon Snow",
+    avatar:
+      "https://images.unsplash.com/photo-1531123897727-8f129e1688ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+
+  {
+    id: 2,
+    username: "Jon Snow",
+    avatar:
+      "https://images.unsplash.com/photo-1531123897727-8f129e1688ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+
+  {
+    id: 3,
+    username: "Jon Snow",
+    avatar:
+      "https://images.unsplash.com/photo-1531123897727-8f129e1688ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+
+  {
+    id: 4,
+    username: "Jon Snow",
+    avatar:
+      "https://images.unsplash.com/photo-1531123897727-8f129e1688ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+
+  {
+    id: 5,
+    username: "Jon Snow",
+    avatar:
+      "https://images.unsplash.com/photo-1531123897727-8f129e1688ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+
+  {
+    id: 6,
+    username: "Jon Snow",
+    avatar:
+      "https://images.unsplash.com/photo-1531123897727-8f129e1688ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+
+  {
+    id: 7,
+    username: "Jon Snow",
+    avatar:
+      "https://images.unsplash.com/photo-1531123897727-8f129e1688ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+];
--- src/pages/userlist/UserList.js
+++ src/pages/userlist/UserList.js
@@ -1,99 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import "./userlist.scss";
 import { DataGrid } from "@mui/x-data-grid";
-import Model from "../../assets/img/model-1.jpg";
+import { DeleteOutline } from "@mui/icons-material";
+import { userRows } from "../../components/data";
+import { Link } from "react-router-dom";
 
 const UserList = () => {
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
   const columns = [
     { field: "id", headerName: "ID", width: 90 },
     {
-      field: "firstName",
-      headerName: "First name",
-      width: 150,
-      editable: true,
+      field: "user",
+      headerName: "User",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="userListUser">
+            <img src={params.row.avatar} alt="" className="userListImg" />
+            {params.row.username}
+          </div>
+        );
+      },
+    },
+    { field: "email", headerName: "email", width: 200 },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 120,
     },
     {
-      field: "lastName",
-      headerName: "Last name",
-      width: 150,
-      editable: true,
-    },
-    {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      width: 110,
-      editable: true,
-    },
-    {
-      field: "fullName",
-      headerName: "Full name",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
+      field: "transaction",
+      headerName: "Transaction Volume",
       width: 160,
     },
-  ];
-
-  const rows = [
     {
-      id: 1,
-      userName: "Jon Snow",
-      avatar: { Model },
-      email: "Jonsnow@gmail.com",
-      status: "active",
-      transaction: $120,
-    },
-
-    {
-      id: 2,
-      userName: "Jon Snow",
-      avatar: { Model },
-      email: "Jonsnow@gmail.com",
-      status: "active",
-      transaction: $120,
-    },
-
-    {
-      id: 3,
-      userName: "Jon Snow",
-      avatar: { Model },
-      email: "Jonsnow@gmail.com",
-      status: "active",
-      transaction: $120,
-    },
-
-    {
-      id: 4,
-      userName: "Jon Snow",
-      avatar: { Model },
-      email: "Jonsnow@gmail.com",
-      status: "active",
-      transaction: $120,
-    },
-
-    {
-      id: 5,
-      userName: "Jon Snow",
-      avatar: { Model },
-      email: "Jonsnow@gmail.com",
-      status: "active",
-      transaction: $120,
-    },
-
-    {
-      id: 6,
-      userName: "Jon Snow",
-      avatar: { Model },
-      email: "Jonsnow@gmail.com",
-      status: "active",
-      transaction: $120,
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/user/" + params.row.id}>
+              <button className="userListEdit">Edit</button>
+              <DeleteOutline
+                className="userListDelete"
+                onClick={() => handleDelete(params.row.id)}
+              />
+            </Link>
+          </>
+        );
+      },
     },
   ];
 
   return (
     <div className="userlist">
       <DataGrid
-        rows={rows}
+        rows={data}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
